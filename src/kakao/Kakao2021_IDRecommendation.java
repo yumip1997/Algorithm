48843c6b89abf307c2be7a52014543de5ba7f544
YUMIPARK <yumip1997@gmail.com>
2022-10-11 10:35:29 +0900

    신규 아이디 추천

---
+++ src/kakao/Kakao2021_IDRecommendation.java
@@ -0,0 +1,28 @@
+package kakao;
+
+public class Kakao2021_IDRecommendation {
+    static class Solution {
+        public String solution(String new_id) {
+            new_id = new_id.toLowerCase();
+            new_id = new_id.replaceAll("[^a-z\\d._\\-]", "");
+            new_id = new_id.replaceAll("[.]{2,}", ".");
+            new_id = new_id.replaceAll("^[.]|[.]$", "");
+
+            if("".equals(new_id)){
+                new_id = "a";
+            }
+
+            if(new_id.length() >= 16){
+                new_id = new_id.substring(0, 15);
+            }
+
+            StringBuilder idBuilder = new StringBuilder(new_id.replaceAll("[.]$", ""));
+
+            while(idBuilder.length() < 3){
+                idBuilder.append(idBuilder.charAt(idBuilder.length() -1));
+            }
+
+            return idBuilder.toString();
+        }
+    }
+}
