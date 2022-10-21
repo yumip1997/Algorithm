fa68f38e2a2fa183435b3a0252bfb83cabdf757c
YUMIPARK <yumip1997@gmail.com>
2022-10-21 11:09:33 +0900

    메뉴 리뉴얼

--- src/순열조합/Kakao2021_Menu.java
+++ src/순열조합/Kakao2021_Menu.java
@@ -5,188 +5,82 @@ import java.util.stream.Collectors;
 
 public class Kakao2021_Menu{
 
-    static class MenuComb implements Comparable<MenuComb>{
-        private Set<Character> menuSet;
-        private int orderCnt;
-        private String menuName;
+    private static final Map<String, Integer> COMB_MENU_CNT_MAP = new HashMap<>();
+    private static final int MINIMUM_COMB_CNT = 2;
 
-        public MenuComb(Set<Character> menuSet){
-            this.menuSet = menuSet;
-            this.orderCnt = 0;
+    public String[] solution(String[] orders, int[] course) {
+        makeMenuCombination(orders, course);
+
+        Map<Integer, List<String>> menuLengthMap = getMenuLengthMap();
+        Map<Integer, Integer> menuLengthOrderCntMap = new HashMap<>();
+
+        for (String combMenu : COMB_MENU_CNT_MAP.keySet()) {
+            int len = combMenu.length();
+            int orderCnt = COMB_MENU_CNT_MAP.get(combMenu);
+            int maxOrderCnt = Math.max(orderCnt, menuLengthOrderCntMap.getOrDefault(len, 0));
+            menuLengthOrderCntMap.put(len, maxOrderCnt);
         }
 
-        public MenuComb(Set<Character> menuSet, int orderCnt) {
-            this.menuSet = menuSet;
-            this.orderCnt = orderCnt;
-        }
-
-        public Set<Character> getMenuSet() {
-            return menuSet;
-        }
-
-        public int getOrderCnt() {
-            return orderCnt;
-        }
-        
-        public boolean is2MoreOrderCnt(){
-            return this.orderCnt >= 2;
-        }
-
-        public boolean isMaxCnt(int maxCnt){
-            return this.orderCnt == maxCnt;
-        }
-
-        private String convertMenuSetToString(){
-            StringBuilder stringBuilder = new StringBuilder();
-            for (Character character : menuSet) {
-                stringBuilder.append(character);
+        List<String> totalMaxOrderMenuCombList = new ArrayList<>();
+        for (Integer len : menuLengthOrderCntMap.keySet()) {
+            Integer maxCnt = menuLengthOrderCntMap.get(len);
+            if(maxCnt < MINIMUM_COMB_CNT){
+                continue;
             }
-            return stringBuilder.toString();
+
+            List<String> maxOrderMenuCombList = getMaxOrderMenuCombList(menuLengthMap, len, maxCnt);
+            totalMaxOrderMenuCombList.addAll(maxOrderMenuCombList);
         }
 
-        public String getMenuName(){
-            if(this.menuName == null){
-                this.menuName = convertMenuSetToString();
+        totalMaxOrderMenuCombList.sort(Comparator.naturalOrder());
+
+        return totalMaxOrderMenuCombList.toArray(new String[0]);
+    }
+
+    private void makeMenuCombination(String[] orders, int[] course) {
+        for (String order : orders) {
+
+            char[] chars = order.toCharArray();
+            Arrays.sort(chars);
+
+            int n = chars.length;
+            for (int r : course) {
+                boolean[] visited = new boolean[n];
+                makeCombination(chars, visited, 0, n, r);
             }
-            return this.menuName;
-        }
-
-        @Override
-        public int compareTo(MenuComb o) {
-            String str = getMenuName();
-            String otherStr = o.getMenuName();
-            return str.compareTo(otherStr);
         }
     }
 
-    public static Map<Integer, List<Set<Character>>> combMap = new HashMap<>();
-
-    public static void makeCombination(char[] arr, boolean[] visited, int depth, int n, int r){
+    private void makeCombination(char[] menus, boolean[] visited, int depth, int n, int r){
         if(r == 0){
-            Set<Character> tempCombSet = new HashSet<>();
+            StringBuilder menuCombStrBuilder = new StringBuilder();
             for(int i=0;i<visited.length;i++){
                 if(visited[i]){
-                    tempCombSet.add(arr[i]);
+                    menuCombStrBuilder.append(menus[i]);
                 }
             }
-            List<Set<Character>> sets = combMap.get(tempCombSet.size());
-            sets.add(tempCombSet);
-            return;
+            String menuCombStr = menuCombStrBuilder.toString();
+            COMB_MENU_CNT_MAP.put(menuCombStr, COMB_MENU_CNT_MAP.getOrDefault(menuCombStr, 0) +1);
         }
 
         for(int i=depth;i<n;i++){
             if(!visited[i]){
                 visited[i] = true;
-                makeCombination(arr, visited, i+1, n, r-1);
+                makeCombination(menus, visited, i+1, n, r-1);
                 visited[i] = false;
             }
         }
     }
 
-    public String[] solution(String[] orders, int[] course) {
-        List<MenuComb> alreadyOrderedMenuCombList = toMenuCombList(orders);
-
-        char[] chars = toDistinctMenuCharArr(orders);
-        int[] combCntArr = getPossibleCombCnt(course, getMaxLength(orders));
-
-        initCombMap(combCntArr);
-
-        int n = chars.length;
-        List<MenuComb> allMaxOrderMenuCombList = new ArrayList<>();
-
-        for (int r : combCntArr) {
-            boolean[] visited = new boolean[n];
-            makeCombination(chars, visited, 0, n, r);
-
-            List<MenuComb> menuCombList = new ArrayList<>();
-            int maxOrderCnt = 0;
-            for (Set<Character> set : combMap.get(r)) {
-                int orderCnt = 0;
-                for (MenuComb menuComb : alreadyOrderedMenuCombList) {
-                    boolean containsAll = menuComb.getMenuSet().containsAll(set);
-                    if(containsAll){
-                        orderCnt++;
-                    }
-                }
-
-                if(orderCnt >= 2 ){
-                    menuCombList.add(new MenuComb(set, orderCnt));
-                }
-
-                maxOrderCnt = Math.max(maxOrderCnt, orderCnt);
-            }
-
-            List<MenuComb> maxOrderedMenuCombList = getMaxOrderedMenuCombList(menuCombList, maxOrderCnt);
-            if(!maxOrderedMenuCombList.isEmpty()){
-                allMaxOrderMenuCombList.addAll(maxOrderedMenuCombList);
-            }
-        }
-
-        allMaxOrderMenuCombList.sort(MenuComb::compareTo);
-
-        return allMaxOrderMenuCombList.stream()
-                .map(MenuComb::getMenuName)
-                .toArray(String[]::new);
-    }
-    
-    private  List<MenuComb> toMenuCombList(String[] orders) {
-        List<MenuComb> menuCombList = new ArrayList<>();
-
-        for (String orderStr : orders) {
-            Set<Character> menuSet = new HashSet<>();
-
-            for(int i=0;i<orderStr.length();i++){
-                menuSet.add(orderStr.charAt(i));
-            }
-
-            menuCombList.add(new MenuComb(menuSet));
-        }
-
-        return menuCombList;
+    private Map<Integer, List<String>> getMenuLengthMap() {
+        return COMB_MENU_CNT_MAP.keySet().stream()
+                .collect(Collectors.groupingBy(String::length));
     }
 
-    private char[] toDistinctMenuCharArr(String[] orders){
-        Set<Character> menuSet = new HashSet<>();
-
-        for (String orderStr : orders) {
-            for(int i=0;i<orderStr.length();i++){
-                menuSet.add(orderStr.charAt(i));
-            }
-        }
-
-        char[] menuCharArr = new char[menuSet.size()];
-        int index=0;
-        for (Character character : menuSet) {
-            menuCharArr[index++] = character;
-        }
-
-        return menuCharArr;
-    }
-
-    private int[] getPossibleCombCnt(int[] course, int maxLength){
-        return Arrays.stream(course)
-                .filter(e -> e<= maxLength)
-                .toArray();
-    }
-
-    private int getMaxLength(String[] orders){
-        int maxLength = 0;
-        for (String order : orders) {
-            int length = order.length();
-            maxLength = Math.max(maxLength, length);
-        }
-        return maxLength;
-    }
-
-    private void initCombMap(int[] combCntArr) {
-        for (int r : combCntArr) {
-            combMap.put(r, new ArrayList<>());
-        }
-    }
-
-    private List<MenuComb> getMaxOrderedMenuCombList(List<MenuComb> menuCombList, int maxOrderCnt) {
-        return menuCombList.stream()
-                .filter(e -> e.isMaxCnt(maxOrderCnt))
+    private List<String> getMaxOrderMenuCombList(Map<Integer, List<String>> menuLengthMap, Integer len, Integer maxCnt) {
+        return menuLengthMap.get(len)
+                .stream()
+                .filter(e -> Objects.equals(COMB_MENU_CNT_MAP.get(e), maxCnt))
                 .collect(Collectors.toList());
     }
 
